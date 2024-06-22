a7e47554dc5f8de513f0aab5eb748f05461b6617
Gustavo Henrique <gustavocarniel718@gmail.com>
2024-06-22 17:11:50 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 export default function Home() {
   return (
-    <div className="p-4">
+    <div className="p-4 bg-slate-900 text-slate-100 h-">
       <h1 className="font-bold text-5xl">Hello Tailwind</h1>
       <h2>Hello Tailwind</h2>
       <p>Hello Tailwind</p>
+      <button className="bg-gray-700 p-2 rounded font-medium mt-4 hover:bg-sky-600">
+        Sign In
+      </button>
     </div>
   );
 }
