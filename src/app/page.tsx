aacaf110d9969e77d9a5c48fd9c6c074e870010c
Gustavo Henrique <gustavocarniel718@gmail.com>
2024-06-23 14:44:57 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 export default function Home() {
   return (
-    <div className="p-4 bg-slate-900 text-slate-100 h-">
-      <h1 className="font-bold text-5xl">Hello Tailwind</h1>
-      <h2>Hello Tailwind</h2>
-      <p>Hello Tailwind</p>
-      <button className="bg-gray-700 p-2 rounded font-medium mt-4 hover:bg-sky-600">
+    <div className="h-screen dark:text-slate-100 dark:bg-slate-950 p-4 bg-slate-50 text-slate-900 flex flex-col items-center text-center">
+      <h1 className="flex font-bold text-5xl sm:text-3xl lg:text-6xl">Hello Tailwind</h1>
+      <h2>Rapidly build modern websites without ever leaving your HTML.</h2>
+      <p className="p-2 mt-4 dark:text-slate-400">A utility-first CSS framework packed with classes like flex, pt-4, text-center and rotate-90 that can be composed to build any design, directly in your markup.</p>
+      <button className="dark:bg-sky-400 bg-gray-700 text-white p-2 rounded font-medium mt-4 hover:bg-sky-600">
         Sign In
       </button>
     </div>
